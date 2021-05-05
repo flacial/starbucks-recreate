2dd5049701e6bfe0898ad1c2b7e3afc1d08afb4c
flacial <ruthemiumwithdiamond@protonmail.com>
2021-05-06 02:02:34 +0400

    structure: beautify files

--- script.js
+++ script.js
@@ -1,121 +1,114 @@
 // navbar link animation start
-const $ = query => document.querySelector(query)
+const $ = (query) => document.querySelector(query);
 
-let line = document.querySelector('#line');
-let item = document.querySelectorAll('#anchor');
+let line = document.querySelector("#line");
+let item = document.querySelectorAll("#anchor");
 
-const tab1 = document.querySelector('#tab-1')
-let secLine = document.querySelector('.secLine');
+const tab1 = document.querySelector("#tab-1");
+let secLine = document.querySelector(".secLine");
 
 // Change secLine width to tab button width
 // secLine.style.width = `${tab1.clientWidth}px`;
 
-let tabs = document.querySelectorAll('.tab');
+let tabs = document.querySelectorAll(".tab");
 
 let header = document.getElementById("nav-links");
 let btns = header.getElementsByClassName("anchor");
 
-const input = $('input');
-const label = $('label');
+const input = $("input");
+const label = $("label");
 
-input.addEventListener('focus', e => {
-    label.style.bottom = '2.2rem';
-    label.classList.add('animate-input-show');
-})
+input.addEventListener("focus", (e) => {
+  label.style.bottom = "2.2rem";
+  label.classList.add("animate-input-show");
+});
 
-input.addEventListener('blur', e => {
-    if (!input.value.length) {
-        label.style.bottom = '10px';
-        label.classList.remove('animate-input-show');
-    }
-})
-
-
-
+input.addEventListener("blur", (e) => {
+  if (!input.value.length) {
+    label.style.bottom = "10px";
+    label.classList.remove("animate-input-show");
+  }
+});
 
 for (var i = 0; i < btns.length; i++) {
-    btns[i].addEventListener("click", function () {
-        var current = document.getElementsByClassName("active");
-        current[0].className = current[0].className.replace(" active", "");
-        this.className += " active";
-    });
+  btns[i].addEventListener("click", function () {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
 }
 
 function indicator(e) {
-    line.style.left = e.offsetLeft + "px";
-    line.style.width = e.offsetWidth + "px";
+  line.style.left = e.offsetLeft + "px";
+  line.style.width = e.offsetWidth + "px";
 }
 
-item.forEach(Link => {
-    Link.addEventListener('click', (e) => {
-        indicator(e.target);
-    })
-})
+item.forEach((Link) => {
+  Link.addEventListener("click", (e) => {
+    indicator(e.target);
+  });
+});
 
 function secIndicator(e) {
-    const mainSecLine = e.childNodes[5];
-    const minionSecLine = e.childNodes[3];
-    secLine.style.left = e.offsetLeft + "px";
+  const mainSecLine = e.childNodes[5];
+  const minionSecLine = e.childNodes[3];
+  secLine.style.left = e.offsetLeft + "px";
 
-    if (minionSecLine?.classList?.[0] === "secLineMinions") {
-        secLine.style.width = minionSecLine.clientWidth + "px";
-    } else {
-        secLine.style.width = mainSecLine.clientWidth + "px"
-    }
+  if (minionSecLine?.classList?.[0] === "secLineMinions") {
+    secLine.style.width = minionSecLine.clientWidth + "px";
+  } else {
+    secLine.style.width = mainSecLine.clientWidth + "px";
+  }
 }
 
-window.addEventListener('resize', e => {
-    secLine.style.width = "100%"
-})
+window.addEventListener("resize", (e) => {
+  secLine.style.width = "100%";
+});
 
 // Make all the tabpanels hidden except the one called tab
 const tabPanelsRemover = (tab) => {
-
-    // Loops through 1 to 6 except the tab number while adding hide to the elements and removing show.
-    for (let i = 1; i < 6; i++) {
-        if (i !== tab) {
-            const tabPanel = document.querySelector(`#tab-panel-${i}`);
-            tabPanel.classList.add('hide');
-            tabPanel.style.position = 'absolute';
-            tabPanel.classList.remove('show');
-            tabPanel.style.zIndex = "";
-        }
+  // Loops through 1 to 6 except the tab number while adding hide to the elements and removing show.
+  for (let i = 1; i < 6; i++) {
+    if (i !== tab) {
+      const tabPanel = document.querySelector(`#tab-panel-${i}`);
+      tabPanel.classList.add("hide");
+      tabPanel.style.position = "absolute";
+      tabPanel.classList.remove("show");
+      tabPanel.style.zIndex = "";
     }
-}
+  }
+};
 
+tabs.forEach((Link) => {
+  Link.addEventListener("click", (e) => {
+    secIndicator(e.target);
 
+    // Slice the last value of the button id which is a number like 1 or 5
+    const id = Number(e.target.id[e.target.id.length - 1]);
 
-tabs.forEach(Link => {
-    Link.addEventListener('click', (e) => {
-        secIndicator(e.target);
+    // Slice the last value of the tab panel id which is also a number like 1 or 5
+    const tabPanelId = (id) => {
+      const tabPanel = document.querySelector(`#tab-panel-${id}`);
 
-        // Slice the last value of the button id which is a number like 1 or 5
-        const id = Number(e.target.id[e.target.id.length - 1])
+      return Number(tabPanel.id[tabPanel.id.length - 1]);
+    };
 
-        // Slice the last value of the tab panel id which is also a number like 1 or 5
-        const tabPanelId = (id) => {
-            const tabPanel = document.querySelector(`#tab-panel-${id}`)
+    // Function to get the tab panel by id
+    const tabPanel = (id) => document.querySelector(`#tab-panel-${id}`);
 
-            return Number(tabPanel.id[tabPanel.id.length - 1])
-        }
-
-        // Function to get the tab panel by id
-        const tabPanel = (id) => document.querySelector(`#tab-panel-${id}`)
-
-        // Checks if button id (1) is equal to the tab id (1), if they're equal add the show class to the tabpanel and remove the hide class and hide all the rest of the tabpanels
-        if (id === tabPanelId(id)) {
-            tabPanel(id).classList.add('show')
-            tabPanel(id).style.position = 'static'
-            tabPanel(id).classList.remove('hide')
-            tabPanel(id).style.zIndex = "400"
-            tabPanelsRemover(id)
-        }
-    })
-})
+    // Checks if button id (1) is equal to the tab id (1), if they're equal add the show class to the tabpanel and remove the hide class and hide all the rest of the tabpanels
+    if (id === tabPanelId(id)) {
+      tabPanel(id).classList.add("show");
+      tabPanel(id).style.position = "static";
+      tabPanel(id).classList.remove("hide");
+      tabPanel(id).style.zIndex = "400";
+      tabPanelsRemover(id);
+    }
+  });
+});
 
 // navbar link animation end
 // drinkImg = document.querySelector('.drink-img');
-
 
 // function imgSlider(images, g) {
 //     document.querySelector('.drink-img').src = images;
